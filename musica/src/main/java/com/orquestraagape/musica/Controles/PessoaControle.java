f0c8423a76e76c47302ed1058d631b33f665ac98
AlexandreTorrecilhas <carlos.torrecilhas@outlook.com>
2025-12-02 20:45:57 -0300

    falta criar a rota delete

--- musica/src/main/java/com/orquestraagape/musica/Controles/PessoaControle.java
+++ musica/src/main/java/com/orquestraagape/musica/Controles/PessoaControle.java
@@ -1,11 +1,14 @@
 package com.orquestraagape.musica.Controles;
 
+import com.orquestraagape.musica.exception.ResourceNotFoundException;
 import com.orquestraagape.musica.modelos.Pessoa;
 import com.orquestraagape.musica.repositorios.PessoaRepositorio;
 import com.orquestraagape.musica.servicos.PessoaServicos;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pessoa")
@@ -23,5 +26,28 @@ public class PessoaControle {
         Pessoa pessoa = this.pessoaServicos.postNovaPessoa(novaPessoa);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(pessoa);
+    }
+
+    @GetMapping("{idPessoa}")
+    public ResponseEntity<Optional<Pessoa>> getPessoaById(@PathVariable int idPessoa){
+
+        Optional<Pessoa> pessoa = this.pessoaServicos.getPessoaById(idPessoa);
+
+        return ResponseEntity.status(HttpStatus.OK).body(pessoa);
+    }
+
+    @PutMapping("/atualizarPessoa/{idPessoa}")
+    public ResponseEntity<Optional<Pessoa>> updatePessoa(
+            @PathVariable int idPessoa,
+            @RequestBody Pessoa novaPessoa
+    ){
+        try{
+            Optional<Pessoa> pessoa = this.pessoaServicos.updatePessoa(idPessoa, novaPessoa);
+
+            return ResponseEntity.status(HttpStatus.OK).body(pessoa);
+
+        }catch(ResourceNotFoundException ex){
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- musica/src/main/java/com/orquestraagape/musica/servicos/PessoaServicos.java
+++ musica/src/main/java/com/orquestraagape/musica/servicos/PessoaServicos.java
@@ -1,8 +1,11 @@
 package com.orquestraagape.musica.servicos;
 
+import com.orquestraagape.musica.exception.ResourceNotFoundException;
 import com.orquestraagape.musica.modelos.Pessoa;
 import com.orquestraagape.musica.repositorios.PessoaRepositorio;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PessoaServicos {
@@ -17,8 +20,24 @@ public class PessoaServicos {
         try{
             return this.pessoaRepositorio.save(pessoa);
         }catch(Exception e){
+            System.out.println("Mensagem: " + e);
             throw(e) ;
         }
+    }
+
+    public Optional<Pessoa> getPessoaById (int idPessoa){
+
+        return this.pessoaRepositorio.findById(idPessoa);
+
+    }
+
+    public Optional<Pessoa> updatePessoa(int idPessoa, Pessoa pessoaAtualizada){
+
+        this.pessoaRepositorio.findById(idPessoa).map(pessoaAtual ->{
+            pessoaAtual.setEmail(pessoaAtualizada.getEmail());
+            return this.pessoaRepositorio.save(pessoaAtual);
+        }).orElseThrow(() -> new ResourceNotFoundException("Esse cadastro não existe"));
+        return Optional.empty();
     }
 
 }
