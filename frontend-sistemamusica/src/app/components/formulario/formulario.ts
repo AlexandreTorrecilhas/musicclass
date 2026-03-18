d8bd70163d55109171172c49eb10ce97ca2c5ce1
AlexandreTorrecilhas <carlos.torrecilhas@outlook.com>
2026-03-17 22:41:16 -0300

    Construindo o formulario, mas ele esta com bug onde carrega somente quando vou clicando nos itens. Criado os servicos para excluir e atualizar responsavel

--- frontend-sistemamusica/src/app/components/formulario/formulario.ts
+++ frontend-sistemamusica/src/app/components/formulario/formulario.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { InterfaceFormulario } from "../../interfaces/interfaceformulario/interfaceFormulario";
 import { CommonModule } from "@angular/common";
 import { CAMPOSDOFORMULARIO } from "./camposformulario/camposformulario";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @Component({
     selector: 'formulario-pessoa',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, ReactiveFormsModule],
     templateUrl: './formulario.html',
     styleUrl: './formulario.css'
 })
